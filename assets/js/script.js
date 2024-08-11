b4e48d482f52558262a86a770a5164e06fdd39d1
ireinhardt00004 <126426396+ireinhardt00004@users.noreply.github.com>
2024-08-12 07:04:07 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,7 +191,6 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-// Start of Tawk.to Live Chat
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
@@ -202,7 +201,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
     s0.parentNode.insertBefore(s1, s0);
 })();
 // End of Tawk.to Live Chat
-// End of Tawk.to Live Chat
+
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
